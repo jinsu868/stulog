7c62a85a706cc8890e7be30a3e4eca38160a3fd1
jinsu868 <mjinsu098@gmail.com>
2024-12-24 00:55:29 +0900

    feat: 유저 도메인 구현 (#1)

---
+++ src/main/java/com/maze/stulog/member/domain/Member.java
@@ -0,0 +1,58 @@
+package com.maze.stulog.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 4098, nullable = false)
+    private String profile;
+
+    @Column(length = 50, nullable = false)
+    private String socialId;
+
+    private Role role;
+
+    public static Member of(
+            String socialId,
+            String name,
+            String profile
+    ) {
+        return new Member(
+                socialId,
+                name,
+                profile
+        );
+    }
+
+    private Member(
+            String socialId,
+            String name,
+            String profile
+    ) {
+        this.socialId = socialId;
+        this.name = name;
+        this.profile = profile;
+        this.role = Role.USER;
+    }
+
+    public boolean isAdmin() {
+        return role == Role.ADMIN;
+    }
+}
