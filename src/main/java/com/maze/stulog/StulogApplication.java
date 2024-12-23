a7d2a6091bcec3cc2fb4bc7758e159b6ed35fbc7
jinsu868 <mjinsu098@gmail.com>
2024-12-23 23:44:26 +0900

    init project

---
+++ src/main/java/com/maze/stulog/StulogApplication.java
@@ -0,0 +1,13 @@
+package com.maze.stulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StulogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StulogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maze/stulog/StulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maze.stulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StulogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
